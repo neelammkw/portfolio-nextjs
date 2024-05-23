781825f4428acadb70fab1bb08676aae162366a1
Neelam Makwana <hiyasoni5@gmail.com>
2024-05-23 13:12:48 +0530

    final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <motion.div>
                   <Link
-                    href="/Resume_Neelam.pdf"
+                    href="/Resume-Neelam.pdf"
                     target={"_blank"}
                     download
                     className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
